dbffa0565113cf22ce0f5f022fd3a106354f9b3a
tk6003 <pahadetarun8@gmail.com>
2025-12-17 14:26:06 +0530

    final 9

--- vite.config.ts
+++ vite.config.ts
@@ -3,31 +3,19 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
-import { metaImagesPlugin } from "./vite-plugin-meta-images";
 
 export default defineConfig({
   plugins: [
     react(),
     runtimeErrorOverlay(),
     tailwindcss(),
-    metaImagesPlugin(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-          await import("@replit/vite-plugin-dev-banner").then((m) =>
-            m.devBanner(),
-          ),
-        ]
-      : []),
+    
   ],
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.dirname, "client", "src"),
-      "@shared": path.resolve(import.meta.dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+    "@": path.resolve(__dirname, "client", "src"),
+    "@shared": path.resolve(__dirname, "shared"),
+    "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
   css: {
@@ -35,9 +23,9 @@ export default defineConfig({
       plugins: [],
     },
   },
-  root: path.resolve(import.meta.dirname, "client"),
+root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+  outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
   },
   server: {
